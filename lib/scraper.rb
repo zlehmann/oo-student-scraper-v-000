44ca2537f7cccb4629c8ef754869222df7e6ee3f
Zachary Lehmann <zlehmann@gmail.com>
2018-10-12 12:30:17 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,10 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  attr_accessor :name, :location, :profile_url
 
+  def initialize
+  end
 
   def self.scrape_index_page(index_url)
     html = open(index_url)
